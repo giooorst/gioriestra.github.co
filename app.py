0fc62e150282c8f780bd146ec89fe9e204fa305b
Giovanna  Riestra Ocampo <gio@Giovannas-MacBook-Air.local>
2024-11-19 11:40:11 +0100

    test

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, jsonify
 
-api_key = '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'
 client = OpenAI(api_key=api_key)
 
 app = Flask(__name__)
